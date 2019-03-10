1ac3c424f063be408720ffd190bec01c6c5ca010
Adam <adam.zouari@heig-vd.ch>
2019-03-10 17:10:16 +0100

    step 2 done

--- src/main/scala/Utils/SpellChecker.scala
+++ src/main/scala/Utils/SpellChecker.scala
@@ -8,7 +8,25 @@ object SpellChecker {
     * @return an integer value, which indicates the Levenshtein distance between "s1" and "s2"
     */
   // TODO - Step 2
-  def stringDistance(s1: String, s2: String): Int = ???
+  // source https://oldfashionedsoftware.com/tag/levenshtein-distance/
+  def stringDistance(s1: String, s2: String): Int = {
+
+    val memo = scala.collection.mutable.Map[(List[Char],List[Char]),Int]()
+
+    def min(a:Int, b:Int, c:Int) = Math.min( Math.min( a, b ), c)
+
+    def sd(s1: List[Char], s2: List[Char]): Int = {
+      if (memo.contains((s1,s2)) == false)
+        memo((s1,s2)) = (s1, s2) match {
+          case (_, Nil) => s1.length
+          case (Nil, _) => s2.length
+          case (c1::t1, c2::t2)  => min( sd(t1,s2) + 1, sd(s1,t2) + 1,
+            sd(t1,t2) + (if (c1==c2) 0 else 1) )
+        }
+      memo((s1,s2))
+    }
+    sd( s1.toList, s2.toList )
+  }
 
   /**
     * Get the syntactically closest word in the dictionary from the given misspelled word, using the "stringDistance"
@@ -17,5 +35,18 @@ object SpellChecker {
     * @return the closest word from "misspelledWord"
     */
   // TODO - Step 2
-  def getClosestWordInDictionary(misspelledWord: String): String = ???
+  def getClosestWordInDictionary(misspelledWord: String): String = {
+
+    // it's a pseudonym or a number
+    if (misspelledWord.startsWith("_") || misspelledWord.matches("[0-9]+"))
+      misspelledWord
+    else{
+
+      var distances = Map[String,Int]()
+      for((k,v) <- Dictionary.dictionary )
+        distances += (k -> stringDistance(misspelledWord,k))
+
+      distances.toSeq.sortBy(_._2).head._1
+    }
+  }
 }
