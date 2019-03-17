4b0bc410858c5e7bfda2bf0cb991cb8066a46b76
Adam <adam.zouari@heig-vd.ch>
2019-03-17 09:32:36 +0100

    remove TODOs

--- src/main/scala/Utils/ClinksCalculator.scala
+++ src/main/scala/Utils/ClinksCalculator.scala
@@ -12,7 +12,6 @@ object ClinksCalculator {
     * @param n the number to compute
     * @return n!
     */
-  // TODO - Step 1
   def factorial(n: Int): Int = factorial(n,1)
 
   private def factorial(n: Int, a: Int): Int = n match {
@@ -25,6 +24,5 @@ object ClinksCalculator {
     * @param k the second number
     * @return n choose k
     */
-  // TODO - Step 1
   def calculateCombination(n: Int, k: Int): Int = factorial(n) /(factorial(k) * factorial(n-k))
 }
